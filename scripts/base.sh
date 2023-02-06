f39757e87cd376cceea424cba18281d1235a7336
AkashiSN <btorntireinvynriy@gmail.com>
2023-02-06 23:54:35 +0900

    bump update library

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -eu
 
+echoerr () {
+  echo "$@" 1>&2;
+}
+
 TARGET_OS="${TARGET_OS:-"Linux"}" # Windows,Darwin,Linux
 
 case ${TARGET_OS} in
 Linux | linux)
+  TARGET_OS="Linux"
   HOST_OS="linux"
   HOST_ARCH=$(uname -m)
   BUILD_TARGET=
@@ -12,22 +17,24 @@ Linux | linux)
   ;;
 Darwin | darwin)
   if [ ! "$(uname)" = "Darwin" ]; then
-    echo 'When TARGET_OS is "Darwin" host must be olso "Darwin"'
+    echoerr 'When TARGET_OS is "Darwin" host must be olso "Darwin"'
     exit 1
   fi
+  TARGET_OS="Darwin"
   HOST_OS="macos"
   HOST_ARCH="universal"
   BUILD_TARGET=
   CROSS_PREFIX=
   ;;
 Windows | windows)
+  TARGET_OS="Windows"
   HOST_OS="linux"
   HOST_ARCH=$(uname -m)
   BUILD_TARGET="x86_64-w64-mingw32"
   CROSS_PREFIX=${BUILD_TARGET}-
   ;;
 *)
-  echo 'TARGET_OS must be "Windows" or "Darwin" or "Linux'
+  echoerr 'TARGET_OS must be "Windows" or "Darwin" or "Linux'
   exit 1
   ;;
 esac
@@ -80,15 +87,15 @@ download_and_unpack_file () {
   local output_name="${2:-$(basename ${url})}"
   local output_dir="$(echo ${output_name} | sed s/\.tar\.*//)"
   if [ ! -e "${output_name}" ]; then
-    echo -n "downloading ${url} ..."
+    echoerr -n "downloading ${url} ..."
     curl -4 "${url}" --retry 50 -o "${output_name}" -L -s --fail
-    echo "done."
+    echoerr "done."
   fi
-  echo -n "unpacking ${output_name} into ${output_dir} ..."
+  echoerr -n "unpacking ${output_name} into ${output_dir} ..."
   rm -rf "${output_dir}"
   mkdir -p "${output_dir}"
   tar -xf "${output_name}" --strip-components 1 -C "${output_dir}"
-  echo "done."
+  echoerr "done."
   cd ${output_dir}
 }
 
@@ -96,11 +103,11 @@ git_clone() {
   cd ${WORKDIR}
   local repo_url="$1"
   local branch="${2:-"master"}"
-  local to_dir="$(basename ${repo_url} | sed s/\.git/_git/)"
-  echo -n "downloading (via git clone) ${to_dir} from $repo_url ..."
+  local to_dir="$(basename ${repo_url} | sed s/\.git//)-${branch}"
+  echoerr -n "downloading (via git clone) ${to_dir} from $repo_url ..."
   rm -rf "${to_dir}"
-  git clone "${repo_url}" -b "${branch}" --depth 1 "${to_dir}"
-  echo "done."
+  git clone -c advice.detachedHead=false "${repo_url}" -b "${branch}" --depth 1 "${to_dir}"
+  echoerr "done."
   cd ${to_dir}
 }
 
@@ -108,10 +115,10 @@ svn_checkout() {
   cd ${WORKDIR}
   local repo_url="$1"
   local to_dir="$(basename ${repo_url})"
-  echo -n "svn checking out to ${to_dir} ..."
+  echoerr -n "svn checking out to ${to_dir} ..."
   svn checkout "${repo_url}" "${to_dir}" --non-interactive --trust-server-cert
   cd ${to_dir}
-  echo "done."
+  echoerr "done."
 }
 
 mkcd () {
@@ -125,32 +132,35 @@ do_configure () {
   local configure_name="${2:-"./configure"}"
 
   if [[ ! -f "${configure_name}" ]]; then
-    if [ -f "bootstrap" ]; then
+    if [ -f "autogen.sh" ]; then
+      export NOCONFIGURE=yes
+      ./autogen.sh
+      unset NOCONFIGURE
+    elif [ -f "bootstrap" ]; then
       ./bootstrap
     elif [ -f "bootstrap.sh" ]; then
       ./bootstrap.sh
-    elif [ -f "autogen.sh" ]; then
-      ./autogen.sh
     else
       autoreconf -fiv
       automake --add-missing
     fi
   fi
 
-  nice -n 5 "${configure_name}" --prefix="${PREFIX}" --host="${BUILD_TARGET}" --enable-static --disable-shared ${configure_options}
+  "${configure_name}" --prefix="${PREFIX}" --host="${BUILD_TARGET}" --enable-static --disable-shared ${configure_options} 1>&2
 }
 
 do_make_and_make_install () {
-  local extra_make_options="${1:-""}"
-  local extra_install_options="${2:-""}"
-  nice make -j ${CPU_NUM} ${extra_make_options}
-  nice make install ${extra_install_options}
+  local overwrite_cpu_num="${1:-${CPU_NUM}}"
+  local extra_make_options="${2:-""}"
+  local extra_install_options="${3:-""}"
+  make -j ${overwrite_cpu_num} ${extra_make_options}
+  make install ${extra_install_options}
 }
 
 do_cmake () {
   local extra_args="${1:-""}"
   local build_from_dir="${2:-"."}"
-  nice -n 5 cmake -G"Unix Makefiles" "${build_from_dir}" -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_TOOLCHAIN_FILE="${WORKDIR}/toolchains.cmake" $extra_args
+  cmake -G"Unix Makefiles" "${build_from_dir}" -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_TOOLCHAIN_FILE="${WORKDIR}/toolchains.cmake" $extra_args
 }
 
 
--- scripts/build-library.sh
+++ scripts/build-library.sh
@@ -7,7 +7,7 @@ source ./base.sh
 #
 
 # Download Cmake
-CMAKE_VERSION=3.23.1
+CMAKE_VERSION=3.25.2
 download_and_unpack_file "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-${HOST_OS}-${HOST_ARCH}.tar.gz"
 case "$(uname)" in
 Darwin)
@@ -47,34 +47,36 @@ fi
 #
 
 # Build zlib
-ZLIB_VERSION=1.2.11
+ZLIB_VERSION="1.2.11"
 download_and_unpack_file "https://download.sourceforge.net/libpng/zlib-${ZLIB_VERSION}.tar.xz"
 CC=${CROSS_PREFIX}gcc AR=${CROSS_PREFIX}ar RANLIB=${CROSS_PREFIX}ranlib ./configure --prefix=${PREFIX} --static
 do_make_and_make_install
 FFMPEG_CONFIGURE_OPTIONS+=("--enable-zlib")
 
 # Build libpng
-LIBPNG_VERSION=1.6.37
+LIBPNG_VERSION="1.6.39"
 download_and_unpack_file "https://download.sourceforge.net/libpng/libpng-${LIBPNG_VERSION}.tar.xz"
 mkcd ${WORKDIR}/libpng_build
 do_cmake "-DPNG_SHARED=0 -DPNG_STATIC=1 -DPNG_TESTS=0" ../libpng-${LIBPNG_VERSION}
 do_make_and_make_install
 
 # Build libjpg
-LIBJPG_VERSION=9e
+LIBJPG_VERSION="9e"
 download_and_unpack_file "http://www.ijg.org/files/jpegsrc.v${LIBJPG_VERSION}.tar.gz"
 do_configure
 do_make_and_make_install
 
 # Build openjpeg
-download_and_unpack_file "https://github.com/uclouvain/openjpeg/archive/master.tar.gz" openjpeg-master.tar.gz
+OPENJPEG_VERSION="2.5.0"
+git_clone "https://github.com/uclouvain/openjpeg.git" v${OPENJPEG_VERSION}
 mkcd ${WORKDIR}/openjpeg_build
-do_cmake "-DBUILD_SHARED_LIBS=0 -DBUILD_TESTING=0 -DBUILD_CODEC=0" ../openjpeg-master
+do_cmake "-DBUILD_SHARED_LIBS=0 -DBUILD_TESTING=0 -DBUILD_CODEC=0" ../openjpeg-v${OPENJPEG_VERSION}
 do_make_and_make_install
 FFMPEG_CONFIGURE_OPTIONS+=("--enable-libopenjpeg")
 
 # Build webp
-git_clone "https://chromium.googlesource.com/webm/libwebp.git"
+WEBP_VERSION="1.3.0"
+git_clone "https://chromium.googlesource.com/webm/libwebp.git" v${WEBP_VERSION}
 export LIBPNG_CONFIG="${PREFIX}/bin/libpng-config --static"
 do_configure "--disable-wic"
 do_make_and_make_install
@@ -82,9 +84,10 @@ unset LIBPNG_CONFIG
 FFMPEG_CONFIGURE_OPTIONS+=("--enable-libwebp")
 
 # Build bzip2
-BZIP2_VERSION=1.0.8
+BZIP2_VERSION="1.0.8"
 download_and_unpack_file "https://www.sourceware.org/pub/bzip2/bzip2-${BZIP2_VERSION}.tar.gz"
-make CC=${CROSS_PREFIX}gcc AR=${CROSS_PREFIX}ar RANLIB=${CROSS_PREFIX}ranlib libbz2.a
+make CC=${CROSS_PREFIX}gcc AR=${CROSS_PREFIX}ar RANLIB=${CROSS_PREFIX}ranlib libbz2.a bzip2
+install -m 755 bzip2 ${PREFIX}/bin
 install -m 644 bzlib.h ${PREFIX}/include
 install -m 644 libbz2.a ${PREFIX}/lib
 cat <<EOS > ${PKG_CONFIG_PATH}/bz2.pc
@@ -102,59 +105,62 @@ Requires:
 Libs: -L\${libdir} -L\${sharedlibdir}
 Cflags: -I\${includedir}
 EOS
+ln -s ${PKG_CONFIG_PATH}/bz2.pc ${PKG_CONFIG_PATH}/bzip2.pc
 
 # Build lzma
-LZMA_VERSION=5.2.5
+LZMA_VERSION="5.4.1"
 download_and_unpack_file "https://sourceforge.net/projects/lzmautils/files/xz-${LZMA_VERSION}.tar.xz"
 do_configure "--disable-xz --disable-xzdec --disable-lzmadec --disable-lzmainfo --disable-scripts --disable-doc"
 do_make_and_make_install
 FFMPEG_CONFIGURE_OPTIONS+=("--enable-lzma")
 
 # Build Nettle
-NETTLE_VERSION=3.7.3
+NETTLE_VERSION="3.8.1"
 download_and_unpack_file "https://ftp.jaist.ac.jp/pub/GNU/nettle/nettle-${NETTLE_VERSION}.tar.gz"
 do_configure "--libdir=${PREFIX}/lib --enable-mini-gmp --disable-openssl --disable-documentation"
 do_make_and_make_install
 
 # Build GMP
-GMP_VERSION=6.2.1
+GMP_VERSION="6.2.1"
 download_and_unpack_file "https://ftp.jaist.ac.jp/pub/GNU/gmp/gmp-${GMP_VERSION}.tar.lz"
 do_configure
 do_make_and_make_install
 FFMPEG_CONFIGURE_OPTIONS+=("--enable-gmp")
 
 # Build Libtasn1
-LIBTASN1_VERSION=4.18.0
+LIBTASN1_VERSION="4.19.0"
 download_and_unpack_file "https://ftp.jaist.ac.jp/pub/GNU/libtasn1/libtasn1-${LIBTASN1_VERSION}.tar.gz"
 do_configure
 do_make_and_make_install
 
 # Build libunistring
-LIBUNISTRING_VERSION=1.0
+LIBUNISTRING_VERSION="1.1"
 download_and_unpack_file "https://ftp.jaist.ac.jp/pub/GNU/libunistring/libunistring-${LIBUNISTRING_VERSION}.tar.xz"
 do_configure
 do_make_and_make_install
 
 # Build iconv
-ICONV_VERSION=1.16
+ICONV_VERSION="1.17"
 download_and_unpack_file "https://ftp.jaist.ac.jp/pub/GNU/libiconv/libiconv-${ICONV_VERSION}.tar.gz"
 do_configure
 make install-lib
 FFMPEG_CONFIGURE_OPTIONS+=("--enable-iconv")
 
 # Build GnuTLS
-GNUTLS_VERSION=3.7.4
-download_and_unpack_file "https://mirrors.dotsrc.org/gcrypt/gnutls/v3.7/gnutls-${GNUTLS_VERSION}.tar.xz"
+GNUTLS_MAJAR_VERSION="3.7"
+GNUTLS_VERSION="${GNUTLS_MAJAR_VERSION}.8"
+download_and_unpack_file "https://mirrors.dotsrc.org/gcrypt/gnutls/v${GNUTLS_MAJAR_VERSION}/gnutls-${GNUTLS_VERSION}.tar.xz"
 do_configure "--disable-tests --disable-doc --disable-tools --without-p11-kit"
 do_make_and_make_install
 FFMPEG_CONFIGURE_OPTIONS+=("--enable-gnutls")
 
 # Build SRT
-download_and_unpack_file "https://github.com/Haivision/srt/archive/master.tar.gz" srt-master.tar.gz
+SRT_VERSION="1.5.1"
+git_clone "https://github.com/Haivision/srt.git" v${SRT_VERSION}
 mkcd ${WORKDIR}/srt_build
 do_cmake "-DENABLE_SHARED=0 -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_BINDIR=bin
           -DCMAKE_INSTALL_INCLUDEDIR=include -DENABLE_APPS=0 -DUSE_STATIC_LIBSTDCXX=1
-          -DUSE_ENCLIB=gnutls" ../srt-master
+          -DUSE_ENCLIB=gnutls" ../srt-v${SRT_VERSION}
 do_make_and_make_install
 FFMPEG_CONFIGURE_OPTIONS+=("--enable-libsrt")
 
@@ -164,7 +170,8 @@ FFMPEG_CONFIGURE_OPTIONS+=("--enable-libsrt")
 #
 
 # Build libvpx
-download_and_unpack_file "https://github.com/webmproject/libvpx/archive/master.tar.gz" libvpx-master.tar.gz
+LIBVPX_VERSION="1.12.0"
+git_clone "https://chromium.googlesource.com/webm/libvpx" v${LIBVPX_VERSION}
 if [ "${TARGET_OS}" = "Windows" ]; then
   CROSS=${CROSS_PREFIX} ./configure --prefix="${PREFIX}" --target=x86_64-win64-gcc --disable-examples --disable-docs --disable-unit-tests --as=yasm
 else
@@ -180,24 +187,24 @@ do_make_and_make_install
 FFMPEG_CONFIGURE_OPTIONS+=("--enable-libx264")
 
 # Build x265
-X265_VERSION=3.5
+X265_VERSION="3.5"
 git_clone "https://bitbucket.org/multicoreware/x265_git" "${X265_VERSION}"
 mkcd ${WORKDIR}/x265_build
 mkdir -p 8bit 10bit 12bit
 
 cd 12bit
-do_cmake "-DHIGH_BIT_DEPTH=1 -DEXPORT_C_API=0 -DENABLE_SHARED=0 -DBUILD_SHARED_LIBS=0 -DENABLE_CLI=0 -DENABLE_TESTS=0 -DMAIN12=1" ../../x265_git/source
+do_cmake "-DHIGH_BIT_DEPTH=1 -DEXPORT_C_API=0 -DENABLE_SHARED=0 -DBUILD_SHARED_LIBS=0 -DENABLE_CLI=0 -DENABLE_TESTS=0 -DMAIN12=1" ../../x265-${X265_VERSION}/source
 make -j ${CPU_NUM}
 cp libx265.a ../8bit/libx265_main12.a
 
 cd ../10bit
-do_cmake "-DHIGH_BIT_DEPTH=1 -DEXPORT_C_API=0 -DENABLE_SHARED=0 -DBUILD_SHARED_LIBS=0 -DENABLE_CLI=0 -DENABLE_TESTS=0" ../../x265_git/source
+do_cmake "-DHIGH_BIT_DEPTH=1 -DEXPORT_C_API=0 -DENABLE_SHARED=0 -DBUILD_SHARED_LIBS=0 -DENABLE_CLI=0 -DENABLE_TESTS=0" ../../x265-${X265_VERSION}/source
 make -j ${CPU_NUM}
 cp libx265.a ../8bit/libx265_main10.a
 
 cd ../8bit
 do_cmake '-DEXTRA_LIB="x265_main10.a;x265_main12.a" -DEXTRA_LINK_FLAGS=-L. -DLINKED_10BIT=1 -DLINKED_12BIT=1
-          -DENABLE_SHARED=0 -DBUILD_SHARED_LIBS=0 -DENABLE_CLI=0 -DENABLE_TESTS=0' ../../x265_git/source
+          -DENABLE_SHARED=0 -DBUILD_SHARED_LIBS=0 -DENABLE_CLI=0 -DENABLE_TESTS=0' ../../x265-${X265_VERSION}/source
 make -j ${CPU_NUM}
 
 mv libx265.a libx265_main.a
@@ -216,14 +223,15 @@ FFMPEG_CONFIGURE_OPTIONS+=("--enable-libx265")
 FFMPEG_EXTRA_LIBS+=("-lpthread" "-lstdc++")
 
 # Build libaom
-git_clone https://aomedia.googlesource.com/aom
+LIBAOM_VERSION="3.5.0"
+git_clone "https://aomedia.googlesource.com/aom" v${LIBAOM_VERSION}
 mkcd ${WORKDIR}/aom_build
 if [ "${TARGET_OS}" = "Windows" ]; then
   cmake -DBUILD_SHARED_LIBS=0 -DENABLE_NASM=1 -DENABLE_DOCS=0 -DENABLE_TESTS=0 -DENABLE_EXAMPLES=0 \
         -DCMAKE_TOOLCHAIN_FILE=../aom/build/cmake/toolchains/x86_64-mingw-gcc.cmake -DAOM_TARGET_CPU=x86_64 \
-        -DCMAKE_INSTALL_PREFIX=${PREFIX} ../aom
+        -DCMAKE_INSTALL_PREFIX=${PREFIX} ../aom-v${LIBAOM_VERSION}
 else
-  do_cmake "-DBUILD_SHARED_LIBS=0 -DENABLE_NASM=1 -DENABLE_DOCS=0 -DENABLE_TESTS=0 -DENABLE_EXAMPLES=0" ../aom
+  do_cmake "-DBUILD_SHARED_LIBS=0 -DENABLE_NASM=1 -DENABLE_DOCS=0 -DENABLE_TESTS=0 -DENABLE_EXAMPLES=0" ../aom-v${LIBAOM_VERSION}
 fi
 do_make_and_make_install
 FFMPEG_CONFIGURE_OPTIONS+=("--enable-libaom")
@@ -234,38 +242,41 @@ FFMPEG_CONFIGURE_OPTIONS+=("--enable-libaom")
 #
 
 # Build opus
-download_and_unpack_file "https://github.com/xiph/opus/archive/master.tar.gz" opus-master.tar.gz
+OPUS_VERSION="1.3.1"
+git_clone "https://github.com/xiph/opus.git" v${OPUS_VERSION}
 mkcd ${WORKDIR}/opus_build
 if [ "${TARGET_OS}" = "Windows" ]; then
-  do_cmake "-DBUILD_SHARED_LIBS=0 -DBUILD_TESTING=0 -DOPUS_STACK_PROTECTOR=0 -DOPUS_FORTIFY_SOURCE=0" ../opus-master
+  do_cmake "-DBUILD_SHARED_LIBS=0 -DBUILD_TESTING=0 -DOPUS_STACK_PROTECTOR=0 -DOPUS_FORTIFY_SOURCE=0" ../opus-v${OPUS_VERSION}
 else
-  do_cmake "-DBUILD_SHARED_LIBS=0 -DBUILD_TESTING=0" ../opus-master
+  do_cmake "-DBUILD_SHARED_LIBS=0 -DBUILD_TESTING=0" ../opus-v${OPUS_VERSION}
 fi
 do_make_and_make_install
 FFMPEG_CONFIGURE_OPTIONS+=("--enable-libopus")
 
 # Build libogg, for vorbis
-download_and_unpack_file "https://github.com/xiph/ogg/archive/master.tar.gz" ogg-master.tar.gz
+OGG_VERSION="1.3.5"
+git_clone "https://github.com/xiph/ogg.git" v${OGG_VERSION}
 mkcd ${WORKDIR}/ogg_build
-do_cmake "-DBUILD_SHARED_LIBS=0 -DINSTALL_CMAKE_PACKAGE_MODULE=0 -DINSTALL_DOCS=0 -DBUILD_TESTING=0" ../ogg-master
+do_cmake "-DBUILD_SHARED_LIBS=0 -DINSTALL_CMAKE_PACKAGE_MODULE=0 -DINSTALL_DOCS=0 -DBUILD_TESTING=0" ../ogg-v${OGG_VERSION}
 do_make_and_make_install
 
 # Build vorbis
-download_and_unpack_file "https://github.com/xiph/vorbis/archive/master.tar.gz" vorbis-master.tar.gz
+VORBIS_VERSION="1.3.7"
+git_clone "https://github.com/xiph/vorbis.git" v${VORBIS_VERSION}
 mkcd ${WORKDIR}/vorbis_build
-do_cmake "-DBUILD_SHARED_LIBS=0 -DINSTALL_CMAKE_PACKAGE_MODULE=0 -DBUILD_TESTING=0" ../vorbis-master
+do_cmake "-DBUILD_SHARED_LIBS=0 -DINSTALL_CMAKE_PACKAGE_MODULE=0 -DBUILD_TESTING=0" ../vorbis-v${VORBIS_VERSION}
 do_make_and_make_install
 FFMPEG_CONFIGURE_OPTIONS+=("--enable-libvorbis")
 
 # Build opencore-amr
-OPENCORE_AMI_VERSION=0.1.5
+OPENCORE_AMI_VERSION="0.1.6"
 download_and_unpack_file "https://download.sourceforge.net/opencore-amr/opencore-amr-${OPENCORE_AMI_VERSION}.tar.gz"
 do_configure
 do_make_and_make_install
 FFMPEG_CONFIGURE_OPTIONS+=("--enable-libopencore-amrnb" "--enable-libopencore-amrwb")
 
 # Build vo-amrwbenc
-VO_AMRWBENC_VERSION=0.1.3
+VO_AMRWBENC_VERSION="0.1.3"
 download_and_unpack_file "https://download.sourceforge.net/opencore-amr/vo-amrwbenc-${VO_AMRWBENC_VERSION}.tar.gz"
 do_configure
 do_make_and_make_install
@@ -283,48 +294,54 @@ FFMPEG_CONFIGURE_OPTIONS+=("--enable-libmp3lame")
 #
 
 # Build freetype
-FREETYPE_VERSION=2.12.0
-download_and_unpack_file "https://download.sourceforge.net/freetype/freetype-${FREETYPE_VERSION}.tar.xz"
-do_configure "--with-zlib=yes --with-png=yes --with-bzip2=yes"
+FREETYPE_VERSION="2-12-1"
+git_clone "https://gitlab.freedesktop.org/freetype/freetype.git" VER-${FREETYPE_VERSION}
+mkcd ${WORKDIR}/freetype_build
+do_cmake "-D BUILD_SHARED_LIBS=0 -D FT_REQUIRE_ZLIB=1 -D FT_REQUIRE_BZIP2=1 -D FT_REQUIRE_PNG=1 -D FT_DISABLE_HARFBUZZ=1" ../freetype-VER-${FREETYPE_VERSION}
 do_make_and_make_install
+sed -i -e "s%Libs: \(.*\)%Libs: \1 -lbz2 -lz -lpng%g" ${PKG_CONFIG_PATH}/freetype2.pc
 FFMPEG_CONFIGURE_OPTIONS+=("--enable-libfreetype")
 
 # Build fribidi
-FRIBIDI_VERSION=1.0.12
-download_and_unpack_file "https://github.com/fribidi/fribidi/releases/download/v${FRIBIDI_VERSION}/fribidi-${FRIBIDI_VERSION}.tar.xz"
+FRIBIDI_VERSION="1.0.12"
+git_clone "https://github.com/fribidi/fribidi.git" v${FRIBIDI_VERSION}
 do_configure "--disable-debug"
-do_make_and_make_install
+do_make_and_make_install 1
 FFMPEG_CONFIGURE_OPTIONS+=("--enable-libfribidi")
 
 # Build libxml2
-LIBXML2_VERSION=2.9.13
-download_and_unpack_file "https://gitlab.gnome.org/GNOME/libxml2/-/archive/v${LIBXML2_VERSION}/libxml2-v${LIBXML2_VERSION}.tar.bz2"
+LIBXML2_VERSION="2.10.3"
+git_clone "https://gitlab.gnome.org/GNOME/libxml2.git" v${LIBXML2_VERSION}
 mkcd ${WORKDIR}/libxml2_build
 do_cmake "-DBUILD_SHARED_LIBS=0 -DLIBXML2_WITH_FTP=0 -DLIBXML2_WITH_HTTP=0 -DLIBXML2_WITH_PYTHON=0 -DLIBXML2_WITH_TESTS=0" ../libxml2-v${LIBXML2_VERSION}
 do_make_and_make_install
 FFMPEG_CONFIGURE_OPTIONS+=("--enable-libxml2")
 
 # Build fontconfig
-FONTCONFIG_VERSION=2.14.0
-download_and_unpack_file "https://www.freedesktop.org/software/fontconfig/release/fontconfig-${FONTCONFIG_VERSION}.tar.xz"
+FONTCONFIG_VERSION="2.14.2"
+git_clone "https://gitlab.freedesktop.org/fontconfig/fontconfig.git" ${FONTCONFIG_VERSION}
 export LIBS="-lm -lz -lbz2 -llzma"
 do_configure "--enable-iconv --enable-libxml2 --disable-docs --with-libiconv"
 do_make_and_make_install
-sed -i -e "s%Libs.private: \(.*\)%Libs.private: \1 -lm -lz -lbz2 -llzma%g" ${PKG_CONFIG_PATH}/fontconfig.pc
+sed -i -e "s%Libs: \(.*\)%Libs: \1 -lm -lz -lbz2 -llzma%g" ${PKG_CONFIG_PATH}/fontconfig.pc
 unset LIBS
 FFMPEG_CONFIGURE_OPTIONS+=("--enable-libfontconfig")
 
 # Build libharfbuzz
-HARFBUZZ_VERSION=4.2.0
-download_and_unpack_file "https://github.com/harfbuzz/harfbuzz/releases/download/${HARFBUZZ_VERSION}/harfbuzz-${HARFBUZZ_VERSION}.tar.xz"
-export LIBS="-lbz2"
+HARFBUZZ_VERSION="6.0.0"
+git_clone "https://github.com/harfbuzz/harfbuzz.git" ${HARFBUZZ_VERSION}
 do_configure "--with-freetype=yes --with-icu=no"
 do_make_and_make_install
-unset LIBS
+
+# Build freetype 2nd
+mkcd ${WORKDIR}/freetype_build
+do_cmake "-D BUILD_SHARED_LIBS=0 -D FT_REQUIRE_ZLIB=1 -D FT_REQUIRE_BZIP2=1 -D FT_REQUIRE_PNG=1 -D FT_REQUIRE_HARFBUZZ=1" ../freetype-VER-${FREETYPE_VERSION}
+do_make_and_make_install
+sed -i -e "s%Libs: \(.*\)%Libs: \1 -lbz2 -lz -lpng -lharfbuzz%g" ${PKG_CONFIG_PATH}/freetype2.pc
 
 # Build libass
-LIBASS_VERSION=0.15.2
-download_and_unpack_file "https://github.com/libass/libass/releases/download/${LIBASS_VERSION}/libass-${LIBASS_VERSION}.tar.xz"
+LIBASS_VERSION="0.17.0"
+git_clone "https://github.com/libass/libass.git" ${LIBASS_VERSION}
 do_configure
 do_make_and_make_install
 if [ "${TARGET_OS}" = "Windows" ]; then
@@ -344,7 +361,7 @@ FFMPEG_CONFIGURE_OPTIONS+=("--enable-libaribb24")
 #
 
 # Build SDL
-SDL_VERSION=2.0.20
+SDL_VERSION="2.26.2"
 download_and_unpack_file "https://www.libsdl.org/release/SDL2-${SDL_VERSION}.tar.gz"
 do_configure
 do_make_and_make_install
@@ -356,7 +373,8 @@ FFMPEG_CONFIGURE_OPTIONS+=("--enable-sdl2")
 #
 
 # Build NVcodec
-download_and_unpack_file "https://github.com/FFmpeg/nv-codec-headers/archive/master.tar.gz" nv-codec-headers-master.tar.gz
+NVCODEC_VERSION="11.1.5.2"
+git_clone "https://git.videolan.org/git/ffmpeg/nv-codec-headers.git" n${NVCODEC_VERSION}
 make install "PREFIX=${PREFIX}"
 FFMPEG_CONFIGURE_OPTIONS+=("--enable-cuda-llvm" "--enable-ffnvcodec" "--enable-cuvid" "--enable-nvdec" "--enable-nvenc")
 
